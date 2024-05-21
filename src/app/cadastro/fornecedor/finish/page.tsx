e9bc7fed01c90c199c50f855aef1a61a87f6de35
Marcio <marciopivajunior457@gmail.com>
2024-05-20 22:49:49 -0300

    Feat: query cep

--- src/app/cadastro/fornecedor/finish/page.tsx
+++ src/app/cadastro/fornecedor/finish/page.tsx
@@ -9,32 +9,69 @@ import { useRouter } from "next/navigation";
 
 export default function Professor(){
   const router = useRouter();
-  const [dataScreenBefore, setDataScreenBefore] = useState(localStorage.getItem("screenbefore"));
-    useEffect(() => {
-      if(!dataScreenBefore){
-        router.push('/cadastro/fornecedor');
-      }
-    }, [dataScreenBefore, router])
+  const [dataScreenAfter, setDataScreenAfter] = useState<any>({});
+  //valores anteriores para teste e enviar para o banco
+  const [dataScreenBefore, setDataScreenBefore] = useState<any>({});
   const formik = useFormik({
         initialValues: {
-            cep: "",
-            estado: "",
-            cidade: "",
-            bairro: "",
-            rua: "",
-            numero: "",
+            cep: dataScreenAfter.cep || "",
+            estado: dataScreenAfter.estado || "",
+            cidade: dataScreenAfter.cidade || "",
+            bairro: dataScreenAfter.bairro || "",
+            rua: dataScreenAfter.rua || "",
+            numero: dataScreenAfter.numero || "",
           },
+          enableReinitialize: true,
         onSubmit: (values) => {
+          localStorage.setItem("screenafter", JSON.stringify(values))
           alert(JSON.stringify(values, null, 2));
         },
       });
+    
+      useEffect(() => {
+        const data = localStorage.getItem("screenafter");
+        if(data){
+          setDataScreenAfter(JSON.parse(data))
+        }
+      }, []);
+
+      useEffect(() => {
+        setDataScreenBefore(localStorage.getItem("screenbefore"));
+        if(!dataScreenBefore){
+          router.push('/cadastro/fornecedor');
+        }
+      }, [dataScreenBefore, router])
+
+      const [cep, setCep] = useState<string>('');
+      const [localidade, setLocalidade] = useState<string>('');
+      const [uf, setUf] = useState<string>('');
+      const [bairro, setBairro] = useState<string>('');
+      useEffect(() => {
+        if (cep) {
+          fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json();
+            })
+            .then((data) => {
+              setLocalidade(data.localidade)
+              setUf(data.uf)
+              setBairro(data.bairro)
+            })
+            .catch((error) => {
+              console.error('Error fetching the data:', error);
+            });
+        }
+      }, [cep]);
     return (
         <Section title="Informe a localização" bottom arrowBefore>
           <DefaultForm handleSubmit={formik.handleSubmit}>
-            <Input type="text" label="CEP" idInput="cep" value={formik.values.cep} onChange={formik.handleChange}/>
-            <Input type="text" label="Estado" idInput="estado" value={formik.values.estado} onChange={formik.handleChange}/>
-            <Input type="text" label="Cidade" idInput="cidade" value={formik.values.cidade} onChange={formik.handleChange}/>
-            <Input type="text" label="Bairro" idInput="bairro" value={formik.values.bairro} onChange={formik.handleChange}/>
+            <Input type="text" label="CEP" idInput="cep" value={formik.values.cep || cep}   onChange={(e) => { formik.handleChange(e); setCep(e.target.value);  }}/>
+            <Input type="text" label="Estado" idInput="estado" value={formik.values.estado || uf} onChange={(e) => { formik.handleChange; setUf(e.target.value) }}/>
+            <Input type="text" label="Cidade" idInput="cidade" value={formik.values.cidade || localidade} onChange={(e) => {formik.handleChange; setLocalidade(e.target.value)}}/>
+            <Input type="text" label="Bairro" idInput="bairro" value={formik.values.bairro || bairro} onChange={(e) => {formik.handleChange; setBairro(e.target.value)}}/>
             <Input type="text" label="Rua" idInput="rua" value={formik.values.rua} onChange={formik.handleChange}/>
             <Input type="text" label="N°" idInput="numero" value={formik.values.numero} onChange={formik.handleChange}/>
             <Button type="submit" is="isNotTransparent">CADASTRAR</Button>
--- src/app/cadastro/fornecedor/page.tsx
+++ src/app/cadastro/fornecedor/page.tsx
@@ -10,17 +10,16 @@ import { useEffect, useRef, useState } from "react";
 export default function Fornecedor(){
     const selectRef = useRef<null | HTMLInputElement>(null);
     const router = useRouter();
-    const [name, setName] = useState('');
-    const [cnpj, setCnpj] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [email, setEmail] = useState('');
+    const [dataBefore, setDataBefore] = useState<any>({});
+
     const formik = useFormik({
         initialValues: {
-            nome: "",
-            cnpj: "",
-            telefone: "",
-            email: "",
+            nome: dataBefore.nome || "",
+            cnpj: dataBefore.cnpj || "",
+            telefone: dataBefore.telefone || "",
+            email: dataBefore.email || "",
         },
+        enableReinitialize: true,
         onSubmit: (values) => {
             localStorage.removeItem("screenbefore");
             const selectedValue = selectRef.current?.value;
@@ -30,17 +29,39 @@ export default function Fornecedor(){
             router.push('/cadastro/fornecedor/finish')
         }
     });
-    const [dataBefore, setDataBefore] = useState<any>('');
+
     useEffect(() => {
-        setDataBefore(localStorage.getItem('screenbefore') ? JSON.parse(localStorage.getItem('screenbefore') as string) : '');
-    }, [])
-    console.log(dataBefore);
+        const storedData = localStorage.getItem('screenbefore');
+        if (storedData) {
+            setDataBefore(JSON.parse(storedData));
+        }
+    }, []);
+
     return(
         <Section title="Cadastre um fornecedor">
             <DefaultForm handleSubmit={formik.handleSubmit}>
-                <Input type="text" label="Nome" idInput="nome" value={dataBefore.nome ? dataBefore.nome : formik.values.nome} onChange={formik.handleChange}></Input>
-                <Input type="text" label="CNPJ" idInput="cnpj" value={dataBefore.cnpj ? dataBefore.cnpj : formik.values.cnpj} onChange={formik.handleChange}></Input>
-                <Input type="text" label="Telefone" selectAside idInput="telefone" value={dataBefore.telefone ? dataBefore.telefone : formik.values.telefone} onChange={formik.handleChange}  optionsFakeSelect={[
+                <Input 
+                    type="text" 
+                    label="Nome" 
+                    idInput="nome" 
+                    value={formik.values.nome} 
+                    onChange={formik.handleChange} 
+                />
+                <Input 
+                    type="text" 
+                    label="CNPJ" 
+                    idInput="cnpj" 
+                    value={formik.values.cnpj} 
+                    onChange={formik.handleChange} 
+                />
+                <Input 
+                    type="text" 
+                    label="Telefone" 
+                    selectAside 
+                    idInput="telefone" 
+                    value={formik.values.telefone} 
+                    onChange={formik.handleChange} 
+                    optionsFakeSelect={[
                         {
                             id: 1,
                             nome: <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_349_2866)"><path d="M14.7989 11.4923C14.76 11.4736 13.3019 10.7556 13.0428 10.6624C12.937 10.6244 12.8237 10.5873 12.7032 10.5873C12.5063 10.5873 12.3409 10.6854 12.2121 10.8782C12.0665 11.0947 11.6256 11.61 11.4893 11.764C11.4715 11.7843 11.4472 11.8086 11.4327 11.8086C11.4196 11.8086 11.194 11.7157 11.1257 11.686C9.56159 11.0066 8.37438 9.37278 8.21159 9.09726C8.18834 9.05765 8.18737 9.03966 8.18717 9.03966C8.19289 9.01869 8.24549 8.96596 8.27263 8.93875C8.35204 8.86018 8.43808 8.75661 8.52133 8.65641C8.56075 8.60894 8.60023 8.56141 8.63899 8.51661C8.75977 8.37609 8.81354 8.267 8.87587 8.14063L8.90854 8.07498C9.06075 7.77258 8.93074 7.51739 8.88873 7.43498C8.85425 7.36602 8.2386 5.88018 8.17315 5.72407C8.01575 5.34739 7.80776 5.172 7.51873 5.172C7.49191 5.172 7.51873 5.172 7.40626 5.17674C7.26932 5.18252 6.52354 5.2807 6.1938 5.48856C5.84412 5.70901 5.25256 6.41174 5.25256 7.64758C5.25256 8.75985 5.95841 9.81005 6.26146 10.2095C6.26899 10.2195 6.28282 10.24 6.30289 10.2693C7.46347 11.9643 8.91029 13.2204 10.377 13.8063C11.789 14.3703 12.4576 14.4355 12.8378 14.4355H12.8378C12.9976 14.4355 13.1254 14.423 13.2382 14.4119L13.3098 14.405C13.7976 14.3618 14.8695 13.8063 15.1133 13.1288C15.3054 12.595 15.3561 12.0119 15.2283 11.8003C15.1407 11.6564 14.9899 11.584 14.7989 11.4923Z" fill="white"/><path d="M10.1775 0C4.76089 0 0.354141 4.37364 0.354141 9.74955C0.354141 11.4883 0.819465 13.1903 1.70096 14.6799L0.0137512 19.6569C-0.0176774 19.7497 0.0056992 19.8523 0.0743356 19.9221C0.123881 19.9727 0.191089 20 0.259725 20C0.286024 20 0.312517 19.996 0.338362 19.9878L5.52797 18.3387C6.9481 19.0975 8.55317 19.498 10.1776 19.498C15.5937 19.4981 20 15.1249 20 9.74955C20 4.37364 15.5937 0 10.1775 0ZM10.1775 17.4671C8.64901 17.4671 7.16856 17.0258 5.89596 16.1907C5.85317 16.1626 5.80349 16.1482 5.75349 16.1482C5.72706 16.1482 5.70057 16.1522 5.67479 16.1604L3.07511 16.9868L3.91434 14.5108C3.94148 14.4307 3.92791 14.3423 3.87791 14.274C2.90882 12.9499 2.39654 11.3854 2.39654 9.74955C2.39654 5.49351 5.88706 2.03091 10.1775 2.03091C14.4673 2.03091 17.9575 5.49351 17.9575 9.74955C17.9575 14.0051 14.4675 17.4671 10.1775 17.4671Z" fill="white"/></g><defs><clipPath id="clip0_349_2866"><rect width="20" height="20" fill="white"/></clipPath></defs></svg>,
@@ -55,10 +76,24 @@ export default function Fornecedor(){
                             active: false,
                             value: "telefone",
                         }
-                    ]} icon selectRef={selectRef}></Input>
-                <Input type="text" label="E-mail" idInput="email" value={dataBefore.email ? dataBefore.email : formik.values.email} onChange={formik.handleChange}></Input>
-                <Button type="submit" is="isNotTransparent">Proximo</Button>
+                    ]}
+                    selectRef={selectRef}
+                />
+                <Input 
+                    label="Email" 
+                    idInput="email" 
+                    type="text"
+                    value={formik.values.email} 
+                    onChange={formik.handleChange} 
+                />
+                <Button 
+                    type="submit" 
+                    is="isNotTransparent"
+                >
+                    Avançar
+                </Button>
+                
             </DefaultForm>
         </Section>
-    )
+    );
 }
