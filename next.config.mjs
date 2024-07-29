31f6851ade0a25f2890aa3bdf03cbe075ba78d63
Marcio <marciopivajunior457@gmail.com>
2024-07-29 20:21:33 -0300

    Test: agente-reajente

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
-    webpack: (config) => {
-        config.resolve.alias['@'] = path.join(__dirname, 'src');
-        return config;
-    },
 };
 
 export default nextConfig;
