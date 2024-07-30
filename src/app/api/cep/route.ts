8fb852aea0b4c4de9285f870bc9bbd2f1871138f
marcio <marciopivajunior457@gmail.com>
2024-07-30 16:28:39 -0300

    Feat: route cep

---
+++ src/app/api/cep/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function POST(res: NextResponse){
+    try{
+        const {cep} = await res.json();
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        if(response){
+            const data = await response.json();
+            console.log(data)
+            return NextResponse.json({message: data}, {status: 200});
+        }
+    } catch(error) {
+        return NextResponse.json({message: `Erro na requisição, tente novamente ${error}`}, {status: 400});
+    }
+}
