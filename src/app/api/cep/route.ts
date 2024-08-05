91f48af903c833fdf282b2894aac7f7de5a182ad
Marcio <marciopivajunior457@gmail.com>
2024-08-05 13:38:45 -0300

    Fix: route cep

--- src/app/api/cep/route.ts
+++ src/app/api/cep/route.ts
@@ -1,16 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(res: NextResponse){
-    try{
-        const {cep} = await res.json();
+export async function POST(request: NextRequest) {
+    try {
+        const { cep } = await request.json();
+
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        if(response.ok){
+    
+        if (response.ok) {
             const data = await response.json();
-            return NextResponse.json({data: data}, {status: 200});
+            return NextResponse.json({ data: data }, { status: 200 });
         } else {
-            return NextResponse.json({message: 'cep inválido'}, {status: 400});
+            return NextResponse.json({ message: 'CEP inválido' }, { status: 400 });
         }
-    } catch(error) {
-        return NextResponse.json({message: `Erro na requisição, tente novamente`}, {status: 400});
+    } catch (error) {
+        return NextResponse.json({ message: 'Erro na requisição, tente novamente' }, { status: 400 });
     }
 }
