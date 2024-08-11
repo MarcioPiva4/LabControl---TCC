59848275ced092005bc9750d13682027804d9976
Marcio <marciopivajunior457@gmail.com>
2024-08-11 02:11:17 -0300

    Fix: erro fornecedores rotas

--- src/app/cadastro/vidrarias/page.tsx
+++ src/app/cadastro/vidrarias/page.tsx
@@ -2,25 +2,8 @@ import VidrariasForm from "@/components/Forms/VidrariasForm";
 import Section from "@/components/Section";
 
 async function getData() {
-    try {
-        const response = await fetch('https://lab-control-g8wb7u7w7-marciop457s-projects.vercel.app/api/fornecedor');
-        const text = await response.json();
-
-        if (response.ok) {
-            try {
-                return JSON.parse(text);
-            } catch (error) {
-                console.error('Failed to parse JSON:', error);
-                return null;
-            }
-        } else {
-            console.error('HTTP Error:', response.status, response.statusText);
-            return null;
-        }
-    } catch (error) {
-        console.error('Fetch error:', error);
-        return null;
-    }
+    const response = await fetch('https://lab-control-g8wb7u7w7-marciop457s-projects.vercel.app/api/fornecedor');
+    return await response.json();
 }
 
 export default async function Equipamentos(){
