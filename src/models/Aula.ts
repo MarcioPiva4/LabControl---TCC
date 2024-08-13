4a63ed3909a4cff9675ba5f04c4237e965adb15a
unknown <marciopivajunior457@gmail.com>
2024-08-13 16:32:02 -0300

    Feat: model aula

--- src/models/Aula.ts
+++ src/models/Aula.ts
@@ -1,20 +1,15 @@
 import { db } from "@/lib/db";
 import { DataTypes } from "sequelize";
+import { Materias } from "./Materias";
+import { Professor } from "./Professor";
+import { Laboratorio } from "./Laboratorio";
 
-const Aula = db.define('aula', {
+const Aula = db.define('aulas', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         allowNull: false,
         primaryKey: true,
-    },
-    materia: { //chave estrangeira
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    professor: {//chave estrangeira
-        type: DataTypes.STRING,
-        allowNull: false,
     },
     topico_aula: {
         type: DataTypes.STRING,
@@ -37,3 +32,104 @@ const Aula = db.define('aula', {
         allowNull: true,
     }
 });
+
+const MateriaAula = db.define('MateriaAula', {
+    id_materia: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Materias,
+            key: 'id'
+        }
+    },
+    id_aula: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Aula,
+            key: 'id'
+        }
+    }
+});
+
+//relacionamentos muitos para muito  - materia/aula
+Materias.belongsToMany(Aula, {
+    through: MateriaAula, 
+    foreignKey: 'id_materia',
+    otherKey: 'id_aula',
+    as: 'aulas',
+});
+
+Aula.belongsToMany(Materias, {
+    through: MateriaAula,
+    foreignKey: 'id_aula',
+    otherKey: 'id_materia',
+    as: 'materias'
+});
+
+
+const ProfessorAula = db.define('MateriaAula', {
+    id_professor: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Materias,
+            key: 'id'
+        }
+    },
+    id_aula: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Aula,
+            key: 'id'
+        }
+    }
+});
+
+//relacionamentos muitos para muito  - professor/aula
+Professor.belongsToMany(Aula, {
+    through: ProfessorAula, 
+    foreignKey: 'id_professor',
+    otherKey: 'id_aula',
+    as: 'aulas',
+});
+
+Aula.belongsToMany(Materias, {
+    through: ProfessorAula,
+    foreignKey: 'id_aula',
+    otherKey: 'id_professor',
+    as: 'professores'
+});
+
+
+const LaboratorioAula = db.define('LaboratorioAula', {
+    id_laboratorio: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Laboratorio,
+            key: 'id'
+        }
+    },
+    id_aula: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Aula,
+            key: 'id'
+        }
+    }
+});
+
+//relacionamentos muitos para muito  - laboratorio/aula
+Professor.belongsToMany(Aula, {
+    through: LaboratorioAula, 
+    foreignKey: 'id_laboratorio',
+    otherKey: 'id_aula',
+    as: 'aulas',
+});
+
+Aula.belongsToMany(Materias, {
+    through: LaboratorioAula,
+    foreignKey: 'id_aula',
+    otherKey: 'id_laboratorio',
+    as: 'professores'
+});
+
+
+export {Aula, MateriaAula, ProfessorAula, LaboratorioAula};
